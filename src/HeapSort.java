05ff5f619084a3eff79a74b0c77dd4a10b20729f
PatMulvihill <mulvihillp993@yahoo.com>
2016-11-11 00:42:07 -0500

    fixed largest number in wrong index issue, off by one error caused the problem. added more test arrays.

--- src/HeapSort.java
+++ src/HeapSort.java
@@ -7,13 +7,13 @@
  * pmulvihill01@manhattan.edu
  * github.com/PatMulvihill/HeapSort
  */
+
 import java.util.Arrays;
 public class HeapSort {
     
     static int heap_size;
     
     public static void buildMaxHeap (int[] A) {
-//        int n = A.length - 1;
         heap_size = A.length - 1;
         for (int i = (heap_size / 2); i >= 0; i--) {
             maxHeapify(A, i);
@@ -40,10 +40,10 @@ public class HeapSort {
     
     public static void heapSort (int[] A) {
         buildMaxHeap(A);
-        for (int i = A.length - 1; i > 0; i--) {
-            swap(A, 1, i);
+        for (int i = heap_size; i > 0; i--) {
+            swap(A, 0, i);
             heap_size -= 1;
-            maxHeapify(A, 1);
+            maxHeapify(A, 0);
         }
     }
 
@@ -55,13 +55,25 @@ public class HeapSort {
     }   
 
     public static void main(String[] args) {
+        
         int[] testArr1 = {16, 4, 10, 14, 7, 9, 3, 2, 8, 1};
         System.out.println("Before HeapSort: testArr1 = " + Arrays.toString(testArr1));
         heapSort(testArr1);
         System.out.println("After HeapSort: testArr1 = " + Arrays.toString(testArr1));
+        
         int[] testArr2 = {-15, 10, 3, 25, -2, 1, 14, 4, 19, 8};
         System.out.println("\nBefore HeapSort: testArr2 = " + Arrays.toString(testArr2));
         heapSort(testArr2);
         System.out.println("After HeapSort: testArr2 = " + Arrays.toString(testArr2));
+                
+        int[] testArr3 = {-16, -4, -10, -14, -7, -9, -3, -2, -8, -1};
+        System.out.println("\nBefore HeapSort: testArr3 = " + Arrays.toString(testArr3));
+        heapSort(testArr3);
+        System.out.println("After HeapSort: testArr3 = " + Arrays.toString(testArr3));
+        
+        int[] testArr4 = {1, 2, 1, 2, 1, 2, 1, 2, 1, 2};
+        System.out.println("\nBefore HeapSort: testArr4 = " + Arrays.toString(testArr4));
+        heapSort(testArr4);
+        System.out.println("After HeapSort: testArr4 = " + Arrays.toString(testArr4));
     }    
 }
